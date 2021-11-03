425e35671ccb647e0acb830b0ef7774ae6aeb717
montieGM <ivy.olson01@gmail.com>
2021-11-02 18:54:56 -0600

    WOOOOO JAVASCRIPT MOVEMENT

--- Homework 9/sketch.js
+++ Homework 9/sketch.js
@@ -1,3 +1,17 @@
+var x1 = 270;
+var x2 = 200;
+var y1 = 500;
+
+var movespeed1 = Math.floor(Math.random() * 10) + 1;
+var movespeed2 = Math.floor(Math.random() * 10) + 1;
+var movespeed3 = Math.floor(Math.random() * 10) + 1;
+
+var titleSize = 15;
+var titleMax = 75
+var titleMin = 3;
+var scaleDir = 1;
+
+
 function setup()
     {
         createCanvas(400, 600);
@@ -6,32 +20,38 @@ function setup()
 function draw()
     {
         background(200);
-        textSize(15);
+        textSize(titleSize);
         fill(0,0,0);
         text("This is ME and definitley not Mario Jumpman Mario.", 10,80);
-        
+        titleSize += 5 * scaleDir;
+
+        if(titleSize >= titleMax || titleSize <= titleMin){
+            scaleDir *= -1;
+        }
+        var y1a = y1 - 300;
+
         //Body
         fill(255,0,0);
-    triangle(100,500,300,500,200,200);
+        triangle(100,y1,300,y1,200,y1a);
         fill(255,255,0);
         point(150,550);
         //Head
         fill(255, 239, 209);
-        circle(200, 250 , 200);
+        circle(x2, 250 , 200);
         fill(255,0,0);
         rect(100,110,200,80);
         rect(200,170,150,20);
         fill(255);
-        circle(250,150,60);
-        circle(300,230,60);
-        circle(220,230,60);
+        circle(x1,150,60);
+        circle(x1,230,60);
+        circle(x1,230,60);
         fill(0);
         textSize(26);
         text("M",240,160);
-        circle(310,230,20);
-        circle(230,230,20);
+        circle(x1,y1,20);
+        circle(x2,y1,20);
         fill(0);
-        ellipse(270,310,100,75)
+        ellipse(x1,310,100,75)
         line(270,330,150,300);
         line(150,300,140,290);
         fill(255, 239, 209);
@@ -41,5 +61,18 @@ function draw()
         textSize(30);
         fill(200,100,0);
         text("Ivy 'Jumpman' Olson", 100, 550)
-        
+
+        x1 += movespeed1;
+        x2 += movespeed2;
+        y1 += movespeed3;
+
+        if(x1 > 350 || x1 < 50){
+            movespeed1 *= -1;
+        }
+        if(x2 > 350 || x2 < 50){
+            movespeed2 *= -1;
+        }
+        if(y1 > 550 || y1 < 50){
+            movespeed3 *= -1;
+        }
     }
